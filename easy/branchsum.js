e07e368fc38b3e69d040a0ba7a5bd5aef64c837a
surprisedPikachu69 <arvind7447@gmail.com>
2023-08-17 16:09:50 +0530

    branch sum input

--- easy/branchsum.js
+++ easy/branchsum.js
@@ -30,3 +30,16 @@ function branchSums(root) {
 
   return res;
 }
+
+let root = new BinaryTree(1);
+root.left = new BinaryTree(2);
+root.right = new BinaryTree(3);
+root.left.left = new BinaryTree(4);
+root.left.right = new BinaryTree(5);
+root.right.left = new BinaryTree(6);
+root.right.right = new BinaryTree(7);
+root.left.left.left = new BinaryTree(8);
+root.left.left.right = new BinaryTree(9);
+root.left.right.left = new BinaryTree(10);
+
+console.log(branchSums(root));
